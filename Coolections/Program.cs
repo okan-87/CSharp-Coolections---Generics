768d9b3e568b6e7c3f6b205bec35a567ca56f951
okan-87 <okankaplan1907@gmail.com>
2022-05-13 17:46:12 +0200

    Add project files.

---
+++ Coolections/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Coolections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[] { "Okan", "Yigit", "Kagan" };
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+            //Arraya sonradan ekleme yapamazsin
+
+            List<string> teams = new List<string> { "Fb","Gs","Bjk","Gb"};
+            foreach (var item in teams)
+            {
+                Console.WriteLine(item);
+            }
+            teams.Add("SS");
+            foreach (var item in teams)
+            {
+                Console.WriteLine(item);
+            }
+
+            
+            
+
+           
+        }
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<String> names = new MyList<String>();
+            names.Add("Okan");
+            Console.WriteLine(names.Length);
+            names.Add("Yigit");
+            Console.WriteLine(names.Length);
+            foreach (var item in names.Items)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
